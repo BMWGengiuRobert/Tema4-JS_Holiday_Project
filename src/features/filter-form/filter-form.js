61a57e8521953507d06b9249b0d614e5ba5af960
BMWGengiuRobert <robert.gengiu@bmwtechworks.ro>
2025-12-29 00:30:58 +0200

    filtering logic created

--- src/features/filter-form/filter-form.js
+++ src/features/filter-form/filter-form.js
@@ -1,1 +1,64 @@
-//  HELPER COMMENT: Here we don't have a starter code snippet :P
+import {
+  getAllEmployees,
+  resetState,
+  setFilteredEmployees,
+} from '../../state/state.js';
+import { renderEmployeeList } from '../employee-list/employee-list.js';
+
+const form = document.getElementById('filter-form');
+const statusFilter = document.getElementById('status-filter');
+const searchFilter = document.getElementById('search-filter');
+const resetButton = document.getElementById('reset-filters');
+const minimumSearchLength = 2;
+
+function applyFilters() {
+  const employees = getAllEmployees();
+  const searchFilterValue = searchFilter.value;
+  const statusFilterValue = statusFilter.value.trim();
+
+  let filteredEmployees = employees;
+
+  if (statusFilterValue !== '') {
+    filteredEmployees = filteredEmployees.filter((employee) => {
+      if (statusFilterValue === 'positive') {
+        return employee.status === 'good';
+      } else if (statusFilterValue === 'negative') {
+        return employee.status === 'naughty';
+      }
+    });
+  }
+
+  if (searchFilterValue.length >= minimumSearchLength) {
+    filteredEmployees = filteredEmployees.filter((employee) => {
+      const nameMatch = employee.name
+        .toLowerCase()
+        .includes(searchFilterValue.toLowerCase());
+      const presentMatch = employee.desiredPresent
+        .toLowerCase()
+        .includes(searchFilterValue.toLowerCase());
+      return nameMatch || presentMatch;
+    });
+  }
+
+  setFilteredEmployees(filteredEmployees);
+  renderEmployeeList();
+}
+
+function handelFormReset() {
+  form.reset();
+  resetState();
+  renderEmployeeList();
+}
+
+export function initializeFilters() {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    applyFilters();
+  });
+
+  statusFilter.addEventListener('change', applyFilters);
+
+  searchFilter.addEventListener('input', applyFilters);
+
+  resetButton.addEventListener('click', handelFormReset);
+}
--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ import { renderEmployeeList } from './features/employee-list/employee-list.js';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import './styles/main.css';
 import './styles/variables.css';
-
-// HELPER COMMENT: Started code to show how to initialize the function from state and features
+import { initializeFilters } from './features/filter-form/filter-form.js';
 
 initializeEmployeesState(employeesData);
-
 renderEmployeeList();
+initializeFilters();
