e3b2ab960df425d760147c0d2545e7ba945fd952
BMWGengiuRobert <robert.gengiu@bmwtechworks.ro>
2025-12-29 02:01:56 +0200

    migrated from css to scss

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import { initializeEmployeesState } from './state/state.js';
 import { renderEmployeeList } from './features/employee-list/employee-list.js';
 
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import './styles/main.css';
-import './styles/variables.css';
+import './styles/main.scss';
 import { initializeFilters } from './features/filter-form/filter-form.js';
 import { setStatistics } from './features/statistics/statistics.js';
 import { initializePagination } from './features/pagination/pagination.js';
